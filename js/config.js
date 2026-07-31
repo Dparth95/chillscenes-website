969a768626e4916d4195ee80927969f597c748af
Dparth95 <dohare.parth@gmail.com>
2026-07-31 09:10:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
    One-time config. After deploying the Apps Script Web App
    (see /apps-script/README-apps-script.md), paste the URL below.
    ============================================================ */
-const APPSCRIPT_URL = "PASTE_YOUR_WEB_APP_URL_HERE";
+const APPSCRIPT_URL = "https://script.google.com/macros/s/AKfycbwfBZKqmIIYi60Vbng65UDUkeK_l6c_O-9O-QLQeUsJNWBuA-3dAdGWnXihu49pdWP3/exec";
